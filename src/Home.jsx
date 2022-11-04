589c0179dd0c20af5d697dd5cdfea3156ce37e57
nono <nono5g@protonmail.com>
2022-11-04 18:21:46 +0100

    first

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Abonnez-vous en masse !</h1>
+      <p>et partagez aussi.</p>
+    </div>
+  );
+};
+
+export default Home;
